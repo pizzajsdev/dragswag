6afee268ae2c3c19d1062ca589cfa723804252a0
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2025-05-14 03:23:00 +0200

    perf: pre-render all docs routes

--- packages/docs/react-router.config.ts
+++ packages/docs/react-router.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from '@react-router/dev/config'
-
+import { examples } from './app/routes/examples/_list'
 export default {
   // Config options...
   // To enable SPA mode we set this to `false`, otherwise it will be SSR by default
@@ -9,4 +9,7 @@ export default {
   },
   // prerender: true,
   basename: import.meta.env['VITE_APP_BASE_PATH'],
+  async prerender() {
+    return ['/', '/examples', ...Object.keys(examples).map((slug) => `/examples/${slug}`)]
+  },
 } satisfies Config
